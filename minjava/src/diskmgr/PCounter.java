7980acc3c942bd4b22e8097a9c30205d6beb9bf1
kushal.bandi <kushal179@gmail.com>
2013-02-22 23:30:18 -0700

    Pcounter added

--- minjava/src/diskmgr/PCounter.java
+++ minjava/src/diskmgr/PCounter.java
@@ -2,12 +2,22 @@ package diskmgr;
 
 public class PCounter {
 
-  public static int counter;
+	public static int counter;
 
-  public static void initialize() {
-      counter =0;
+	public static void initialize() {
+		counter = 0;
 	}
-  public static void increment() {
-      counter++;
-    }
+
+	public static void increment() {
+		counter++;
+	}
+
+	public static int getCounter() {
+		return counter;
+	}
+
+	public static void setCounter(int counter) {
+		PCounter.counter = counter;
+	}
+
 }
